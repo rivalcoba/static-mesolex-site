12e886491ac7088ee7d9766793d66939679c0c02
rivalcoba <ivan.rivalcoba@gmail.com>
2021-11-07 02:10:18 -0600

    Removing comments

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -21,45 +21,13 @@
         $t('navBar.help')
       }}</nuxt-link>
       <!-- Language selector -->
-      <the-language-selector/>
-      <!-- <div class="contenedor-selector-leng">
-        <div class="menu-item-vertical-line"></div>
-        <button
-          :class="[
-            isSpanishActive ? 'selector-lenguaje-activo' : 'selector-lenguaje',
-          ]"
-          @click="setLocale($i18n.locales[0].code)"
-        >
-          {{ $i18n.locales[0].name }}
-        </button>
-        <p class="divisor-selector-lenguaje">|</p>
-        <button
-          :class="[
-            !isSpanishActive ? 'selector-lenguaje-activo' : 'selector-lenguaje',
-          ]"
-          @click="setLocale($i18n.locales[1].code)"
-        >
-          {{ $i18n.locales[1].name }}
-        </button>
-      </div> -->
+      <the-language-selector />
     </nav>
   </header>
 </template>
 
 <script>
-export default {
-  // computed: {
-  //   isSpanishActive() {
-  //     return this.$i18n.locale === 'es';
-  //   },
-  // },
-  // methods: {
-  //   setLocale(locale) {
-  //     this.$i18n.locale = locale;
-  //     localStorage.setItem('locale', locale);
-  //   },
-  // },
-};
+export default {};
 </script>
 
 <style></style>
